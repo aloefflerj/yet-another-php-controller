3e9f843d76ca5f5d5002d746d82267ef4effcdd8
Anderson <aloeffler.j@gmail.com>
2022-05-02 00:31:14 -0300

    test: psr7 messate 'get protocol version' test added

---
+++ tests/Controller/unit/MessageTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Aloefflerj\YetAnotherController;
+
+use Aloefflerj\YetAnotherController\Controller\Http\Message;
+use Aloefflerj\YetAnotherController\Controller\Http\Stream;
+use PHPUnit\Framework\TestCase;
+
+class MessageTest extends TestCase
+{
+    public function testGetProtocolVersion(): void
+    {
+        $message = new Message();
+        $this->assertEquals('1.0', $message->getProtocolVersion());
+        
+        $message = $message->withProtocolVersion('1.1');
+        $this->assertEquals('1.1', $message->getProtocolVersion());
+    }
+}
