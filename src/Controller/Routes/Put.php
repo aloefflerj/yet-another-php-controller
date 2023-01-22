9ee30b5c823b9309304297b3c84325e220b9993a
Anderson Job Loeffler <aloeffler.j@gmail.com>
2023-01-22 15:26:40 -0300

    :recycle: refactor: fix non declared class attributes deprecation

--- src/Controller/Routes/Put.php
+++ src/Controller/Routes/Put.php
@@ -4,13 +4,15 @@ namespace Aloefflerj\YetAnotherController\Controller\Routes;
 
 class Put extends Route implements RouteInterface
 {
+    private string | false $body;
+
     public function __construct(string $uri, \closure $output, ?array $functionParams)
     {
         parent::__construct($uri, $output, $functionParams);
 
         $this->method         =  parent::getMethodName(__CLASS__);
         $this->headerParams   = $this->splitToParams($uri);
-        $this->body           = file_get_contents('php://input', true); 
+        $this->body           = file_get_contents('php://input', true);
     }
 
     public static function getRoute(string $currentUri, array $routes, string $currentRequestMethod): string
