a1090b1d5eb704ab169aea894a4d057e13bfc0e5
Anderson Job Loeffler <aloeffler.j@gmail.com>
2023-01-27 20:04:55 -0300

    :white_check_mark: test: adds message headers test

--- tests/Controller/unit/MessageTest.php
+++ tests/Controller/unit/MessageTest.php
@@ -14,10 +14,27 @@ class MessageTest extends TestCase
     {
         $message = new Message();
         $this->assertEquals('1.0', $message->getProtocolVersion());
-        
+
         $message = $message->withProtocolVersion('1.1');
         $this->assertEquals('1.1', $message->getProtocolVersion());
     }
 
+    public function testHeaders(): void
+    {
+        $message = new Message();
+        $this->assertEquals([], $message->getHeaders());
+        $this->assertEquals(false, $message->hasHeader('content-type'));
+        $this->assertEquals([], $message->getHeader('content-type'));
+        $this->assertEquals('', $message->getHeaderLine('content-type'));
 
+        $message = $message->withHeader('content-type', 'application/json');
+        $message = $message->withHeader('user-agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)');
+
+        $settedHeader = [
+            'content-type' => 'application/json',
+            'user-agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+        ];
+
+        $this->assertEquals($settedHeader, $message->getHeaders());
+    }
 }
