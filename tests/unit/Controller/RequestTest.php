05b1580228a5ee83099bdca44440f6a04fd1e24b
Anderson Job Loeffler <aloeffler.j@gmail.com>
2023-02-02 11:29:47 -0300

    :recycle: refactor: removes unnecessary imports

--- tests/unit/Controller/RequestTest.php
+++ tests/unit/Controller/RequestTest.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace Aloefflerj\YetAnotherController;
 
-use Aloefflerj\YetAnotherController\Controller\Http\Message;
 use Aloefflerj\YetAnotherController\Controller\Http\Request;
-use Aloefflerj\YetAnotherController\Controller\Http\Stream;
 use PHPUnit\Framework\TestCase;
 
 class RequestTest extends TestCase
