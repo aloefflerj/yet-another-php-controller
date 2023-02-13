1ec77655fe04c70be3fbf88686e0d94daeb6bf93
Anderson Job Loeffler <aloeffler.j@gmail.com>
2023-02-13 19:55:28 -0300

    :heavy_check_mark: test: passes uploaded file stream test

--- tests/unit/Controller/UploadedFileTest.php
+++ tests/unit/Controller/UploadedFileTest.php
@@ -4,15 +4,17 @@ declare(strict_types=1);
 
 namespace Aloefflerj\YetAnotherController;
 
-use Aloefflerj\YetAnotherController\Controller\Http\Message;
 use Aloefflerj\YetAnotherController\Controller\Http\Stream;
+use Aloefflerj\YetAnotherController\Controller\Http\UploadedFile;
 use PHPUnit\Framework\TestCase;
 
 class UploadedFileTest extends TestCase
 {
+    const FILE_PATH = __DIR__ . '/StreamDummyFile.txt';
+    
     public function testStream(): void
     {
-        $dummyFile = fopen('./StreamDummyFile.txt', 'r+');
+        $dummyFile = fopen(self::FILE_PATH, 'r+');
         $stream = new Stream($dummyFile);
         $uploadedFile = new UploadedFile($stream);
         $this->assertEquals($stream, $uploadedFile->getStream());
