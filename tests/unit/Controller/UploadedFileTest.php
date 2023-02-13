2350c217f655b3226ffc75b96ba506a994085323
Anderson Job Loeffler <aloeffler.j@gmail.com>
2023-02-13 20:01:44 -0300

    :white_check_mark: test: adds uploaded file move to test

--- tests/unit/Controller/UploadedFileTest.php
+++ tests/unit/Controller/UploadedFileTest.php
@@ -10,13 +10,27 @@ use PHPUnit\Framework\TestCase;
 
 class UploadedFileTest extends TestCase
 {
+    const FILE_NAME = __DIR__ . 'StreamDummyFile.txt';
     const FILE_PATH = __DIR__ . '/StreamDummyFile.txt';
-    
+    const EMPTY_DIR = __DIR__ . '/emptyDir';
+
     public function testStream(): void
     {
         $dummyFile = fopen(self::FILE_PATH, 'r+');
         $stream = new Stream($dummyFile);
         $uploadedFile = new UploadedFile($stream);
         $this->assertEquals($stream, $uploadedFile->getStream());
+    }
+
+    public function testMoveTo(): void
+    {
+        $dummyFile = fopen(self::FILE_PATH, 'r+');
+        $stream = new Stream($dummyFile);
+        $uploadedFile = new UploadedFile($stream);
+        $uploadedFile->moveTo(self::EMPTY_DIR);
+        $this->assertTrue(file_exists(self::EMPTY_DIR . '/' . self::FILE_NAME));
+        $this->assertFalse(file_exists(self::FILE_PATH));
+
+        $uploadedFile->moveTo(__DIR__);
     }
 }
