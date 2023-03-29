b605ac9132f942cf8683af9e4f6e62e13776cd56
Anderson Job Loeffler <aloeffler.j@gmail.com>
2023-03-28 23:08:43 -0300

    :sparkles: feat: adds uploaded file move to implementation

--- src/Controller/Http/UploadedFile.php
+++ src/Controller/Http/UploadedFile.php
@@ -14,11 +14,30 @@ class UploadedFile #implements UploadedFileInterface
         private ?int $size = null,
         private int $error = 0
     ) {
-        
     }
 
     public function getStream()
     {
         return $this->source;
+    }
+
+    public function moveTo($targetPath)
+    {
+        $dirPath = dirname($targetPath);
+        if (!is_dir($dirPath)) {
+            throw new \InvalidArgumentException("The given path '{$targetPath}' is not a valid path. Dir '$dirPath' does not exists");
+        }
+
+        if (!is_writable($dirPath)) {
+            throw new \RuntimeException("The dir path '{$targetPath}' is not writable.");
+        }
+
+        $sourcePath = $this->source->getMetadata('uri');
+        if (!rename($sourcePath, $targetPath)) {
+            $errorMsg = "There was an error when trying to move the file.\n";
+            $errorMsg .= "Source path: {$sourcePath}\n";
+            $errorMsg .= "Target path: {$targetPath}\n";
+            throw new \RuntimeException($errorMsg);
+        }
     }
 }
