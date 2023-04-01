5f34d84d7ed5cb556aee40222f4357272165d08e
Anderson Job Loeffler <aloeffler.j@gmail.com>
2023-04-01 20:43:52 -0300

    :recycle: refactor: adds fixtures to uploaded files test

--- tests/unit/Controller/UploadedFileTest.php
+++ tests/unit/Controller/UploadedFileTest.php
@@ -7,6 +7,7 @@ namespace Aloefflerj\YetAnotherController;
 use Aloefflerj\YetAnotherController\Controller\Http\Stream;
 use Aloefflerj\YetAnotherController\Controller\Http\UploadedFile;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\StreamInterface;
 
 class UploadedFileTest extends TestCase
 {
@@ -14,39 +15,47 @@ class UploadedFileTest extends TestCase
     const FILE_PATH = __DIR__ . '/StreamDummyFile.txt';
     const EMPTY_DIR = __DIR__ . '/emptyDir';
 
+    private mixed $dummyFile;
+    private StreamInterface $stream;
+    private UploadedFile $uploadedFile;
+
+    protected function setUp(): void
+    {
+        $this->dummyFile = fopen(self::FILE_PATH, 'r+');
+        $this->stream = new Stream($this->dummyFile);
+        $this->uploadedFile = new UploadedFile($this->stream);
+    }
+
     public function testStream(): void
     {
-        $dummyFile = fopen(self::FILE_PATH, 'r+');
-        $stream = new Stream($dummyFile);
-        $uploadedFile = new UploadedFile($stream);
+        $stream = $this->stream;
+        $uploadedFile = $this->uploadedFile;
+
         $this->assertEquals($stream, $uploadedFile->getStream());
     }
 
     public function testMoveTo(): void
     {
-        $dummyFile = fopen(self::FILE_PATH, 'r+');
-        $stream = new Stream($dummyFile);
-        $uploadedFile = new UploadedFile($stream);
+        $uploadedFile = $this->uploadedFile;
+
         $uploadedFile->moveTo(self::EMPTY_DIR . '/' . self::FILE_NAME);
         $this->assertTrue(file_exists(self::EMPTY_DIR . '/' . self::FILE_NAME));
         $this->assertFalse(file_exists(self::FILE_PATH));
 
         $this->returnFileToOriginalPath();
     }
-    
+
     public function testGetSize(): void
     {
-        $dummyFile = fopen(self::FILE_PATH, 'r+');
-        $stream = new Stream($dummyFile);
-        $uploadedFile = new UploadedFile($stream);
+        $uploadedFile = $this->uploadedFile;
+
         $this->assertEquals(56, $uploadedFile->getSize());
     }
 
     public function testGetError(): void
     {
-        $dummyFile = fopen(self::FILE_PATH, 'r+');
-        $stream = new Stream($dummyFile);
-        $uploadedFile = new UploadedFile($stream);
+        $uploadedFile = $this->uploadedFile;
+
         $this->assertEquals(0, $uploadedFile->getError());
     }
 
@@ -54,5 +63,4 @@ class UploadedFileTest extends TestCase
     {
         rename(self::EMPTY_DIR . '/' . self::FILE_NAME, __DIR__ . '/' . self::FILE_NAME);
     }
-
 }
