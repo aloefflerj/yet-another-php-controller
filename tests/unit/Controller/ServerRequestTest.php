ae2555c2a7dbf9a7eb504d64faca791e253056c1
Anderson Job Loeffler <aloeffler.j@gmail.com>
2023-04-02 14:59:52 -0300

    :fire: style: removes unnecessary class imports

--- tests/unit/Controller/ServerRequestTest.php
+++ tests/unit/Controller/ServerRequestTest.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Aloefflerj\YetAnotherController;
 
-use Aloefflerj\YetAnotherController\Controller\Http\Method;
-use Aloefflerj\YetAnotherController\Controller\Http\Request;
 use Aloefflerj\YetAnotherController\Controller\Http\ServerRequest;
 use Aloefflerj\YetAnotherController\Controller\Http\Stream;
 use Aloefflerj\YetAnotherController\Controller\Http\Uri;
