0990860339ad0b04eea22be1452e5248684e3f15
Anderson Job Loeffler <aloeffler.j@gmail.com>
2023-04-05 21:20:25 -0300

    :white_check_mark: test: adds psr response instantiation test

---
+++ tests/unit/Controller/ResponseTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Aloefflerj\YetAnotherController;
+
+use Aloefflerj\YetAnotherController\Controller\Http\Stream;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
+
+class ResponseTest extends TestCase
+{
+    #[DataProvider('responseCasesProvider')]
+    public function testResponseIsCorrectlyInstantiated(
+        int $status,
+        array $headers,
+        StreamInterface $body,
+        string $version,
+        string $reason
+    ): void {
+        $response = new Response(
+            $status,
+            $headers,
+            $body,
+            $version,
+            $reason
+        );
+
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+    }
+
+    public static function responseCasesProvider(): array
+    {
+        return ['simple-response-case' => [
+            'statusCode' => 200,
+            'headers' => ['content-type' => ['application/json']],
+            'body' => Stream::buildFromString(
+                json_encode((object)['user' => 'holy jesus'])
+            ),
+            'version' => '1.1',
+            'reason' => 'OK'
+        ]];
+    }
+}
