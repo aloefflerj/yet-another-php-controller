b6d872f6b507e468fb59b0d4e70d7f85f6521dee
Anderson Job Loeffler <aloeffler.j@gmail.com>
2023-04-07 01:40:59 -0300

    :sparkles: feat: implements response with status

--- src/Controller/Http/Response.php
+++ src/Controller/Http/Response.php
@@ -18,7 +18,7 @@ class Response extends Message #implements ResponseInterface
     ) {
         if (!$this->isStatusCodeValid($status))
             throw new \InvalidArgumentException('Invalid status code');
-            
+
         parent::__construct($headers, $protocolVersion, $body);
     }
 
@@ -34,7 +34,23 @@ class Response extends Message #implements ResponseInterface
         if (empty($reason)) {
             return $this->getReasonPhraseByCode($this->status);
         }
-        
+
         return $this->reason;
+    }
+
+    public function withStatus($code, $reasonPhrase = '')
+    {
+        if (!$this->isStatusCodeValid($code))
+            throw new \InvalidArgumentException('Invalid status code');
+
+        if (empty($reasonPhrase)) {
+            $reasonPhrase = $this->getReasonPhraseByCode($code);
+        }
+
+        $clone = clone $this;
+        $clone->status = $code;
+        $clone->reason = $reasonPhrase;
+
+        return $clone;
     }
 }
