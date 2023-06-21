88d47b6ddb25dbd69be8634d4f44c2683cb71759
Anderson Job Loeffler <aloeffler.j@gmail.com>
2023-06-21 20:12:59 -0300

    :heavy_check_mark: test: passes route entity fetch tests

--- tests/acceptance/Controller/RoutesTest.php
+++ tests/acceptance/Controller/RoutesTest.php
@@ -7,6 +7,7 @@ namespace Aloefflerj\YetAnotherController;
 use Aloefflerj\YetAnotherController\Controller\BaseController;
 use Aloefflerj\YetAnotherController\Controller\Http\Message;
 use Aloefflerj\YetAnotherController\Controller\Http\Stream;
+use Aloefflerj\YetAnotherController\Tests\Helpers\TestClient;
 use Aloefflerj\YetAnotherController\Tests\Helpers\WebServerHelper;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
@@ -16,6 +17,7 @@ use Psr\Http\Message\StreamInterface;
 class RoutesTest extends TestCase
 {
     private ?WebServerHelper $server = null;
+    private TestClient $client;
 
     const MASCOTS_FIXTURE = [
         [
@@ -34,20 +36,38 @@ class RoutesTest extends TestCase
     {
         $this->server = new WebServerHelper('localhost:9000', '', 'RoutesTest', 'routeProvider');
         $this->server->startWebServer();
+
+        $this->client = $this->server->makeClient();
     }
 
     public function testHomeRoute(): void
     {
-        $client = $this->server->makeClient();
-        $response = $client->testRequest('GET', '');
+        $response = $this->client->testRequest('GET', '');
         $this->assertEquals('home', strval($response->getBody()));
     }
 
-    public function testJsonReturnRoute(): void
+    public function testGetMascotsReturnsAListOfMascots(): void
     {
-        $client = $this->server->makeClient();
-        $response = $client->testRequest('GET', 'mascots');
+        $response = $this->client->testRequest('GET', 'mascots');
         $this->assertEquals(json_encode(self::MASCOTS_FIXTURE), strval($response->getBody()));
+    }
+
+    public function testGetEntityByIdReturnsTheSearchedEntity(): void
+    {
+        $mascotId = 1;
+        
+        $response = $this->client->testRequest('GET', "mascots/{$mascotId}");
+        $foundMascotIndex = array_search(1, array_column(self::MASCOTS_FIXTURE, 'id'));
+        $foundMascot = self::MASCOTS_FIXTURE[$foundMascotIndex];
+        $this->assertEquals(json_encode($foundMascot), strval($response->getBody()));
+    }
+    
+    public function testNotFoundGetEntityByIdReturnsEmptyMascot(): void
+    {
+        $mascotId = 1000;
+
+        $response = $this->client->testRequest('GET', "mascots/{$mascotId}");
+        $this->assertEmpty(json_decode(strval($response->getBody())));
     }
 
     public function routeProvider(): \closure
@@ -61,6 +81,17 @@ class RoutesTest extends TestCase
 
             $app->get('/mascots', function ($req, $res, $headerParams, $functionParams) {
                 echo json_encode(self::MASCOTS_FIXTURE);
+            });
+
+            $app->get('/mascots/{id}', function ($req, $res, $headerParams, $functionParams) {
+                $foundMascotIndex = array_search($headerParams->id, array_column(self::MASCOTS_FIXTURE, 'id'));
+                if ($foundMascotIndex === false) {
+                    echo json_encode([]);
+                    return;
+                }
+
+                $foundMascot = self::MASCOTS_FIXTURE[$foundMascotIndex] ?? [];
+                echo json_encode($foundMascot);
             });
 
             $app->dispatch();
