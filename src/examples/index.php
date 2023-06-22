fcbeaeeca8abbf9505c2e9535a9ecc2458568569
Anderson Job Loeffler <aloeffler.j@gmail.com>
2023-06-21 22:17:57 -0300

    :test_tube: test: adds dispatch and routes to index server testing

--- src/examples/index.php
+++ src/examples/index.php
@@ -3,10 +3,12 @@
 declare(strict_types=1);
 
 use Aloefflerj\YetAnotherController\Controller\Controller;
+use Psr\Http\Message\RequestInterface;
 
 require_once dirname(__DIR__, 2) . '/vendor/autoload.php';
 
 $controller = new Controller('http://localhost:8000');
-$controller->get('/oi', function () { return; });
+$controller->get('/', function (RequestInterface $request) { echo '<pre>', var_dump($request->getBody()->getContents()), '</pre>'; });
+$controller->post('/', function (RequestInterface $request) { echo ($request->getBody()->getContents()); });
 
-dd($controller);
+$controller->dispatch();
